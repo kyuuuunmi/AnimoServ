392fc3b9f312d7e08e548a322d19d23dccd86be1
yuuuunmi <kyuuuunmi@gmail.com>
2017-05-02 17:33:58 +0900

    add routing darknet using CNN

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
 const express = require('express');
-const path = require('path');
 const index = require('./routes/index');
 const screens = require('./routes/screen');
-
+const darknet = require('./routes/darknet');
 const app = express();
 
 
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
-//app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static('public')); // 정적인 파일이 위치할 디렉토리를 지정하는 기능
-
 
 app.use('/', index);
 app.use('/screen', screens);
-
+app.use('/darknet', darknet);
 
 
 module.exports = app;
---
+++ routes/darknet.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+const path = require('path');
+const _ = require('underscore');
+const async = require('async');
+
+var exec = require('child_process').exec, child;
+
+router.get('/', function(req, res){
+
+  // 1) 맨 마지막 raw image를 불러온다 & 명령행 인자 선언
+  var raw_image = getMostRecentFileName('/home/ubuntu/CNN/raw_image');
+  var arg_darknet = "../../CNN/Animo_darknet/Animo_Darknet/Animo_Darknet/darknet detector test cfg/voc.data cfg/tiny-yolo-voc.cfg tiny-yolo-voc.weights " + raw_image;
+  var arg_mv = "mv /home/ubuntu/CNN/Animo_darknet/Animo_Darknet/Animo_Darknet/predictions.png /home/ubuntu/CNN/dt_image/"+ Date.now() + ".png";
+
+  async.series([
+
+    // 2) 다크넷에 파일 인자를 넣어 실행.
+    function(callback){
+
+      child = exec(arg_darknet, function (error, stdout, stderr) {
+          console.log('stdout: ' + stdout);
+          console.log('stderr: ' + stderr);
+          if (error !== null) {
+              console.log('exec error: ' + error);
+          }
+      });
+    },
+    // 3) output은 해당 경로로 이동시켜 준다.
+    function(callback){
+
+      child = exec(arg_mv, function (error, stdout, stderr) {
+          console.log('stdout: ' + stdout);
+          console.log('stderr: ' + stderr);
+          if (error !== null) {
+              console.log('exec error: ' + error);
+          }
+      });
+    }], function(err, result){
+      if (err)
+        console.log(err);
+      else
+        res.send("successed");
+  });
+});
+function getMostRecentFileName(dir) {
+    var files = fs.readdirSync(dir);
+
+    // use underscore for max
+    // 가장 최근에 변경된 파일을 리턴해줌
+    return path.join(dir, _.max(files, function (f) {
+        var fullpath = path.join(dir, f); // 경로+파일 이름
+
+        // ctime = creation time is used
+        // replace with mtime for modification time
+        return fs.statSync(fullpath).ctime;
+    }));
+}
+
+
+/*
+var fs = require('fs'),
+    path = require('path'),
+    _ = require('underscore');
+
+// Return only base file name without dir
+function getMostRecentFileName(dir) {
+    var files = fs.readdirSync(dir);
+
+    // use underscore for max()
+    return _.max(files, function (f) {
+        var fullpath = path.join(dir, f);
+
+        // ctime = creation time is used
+        // replace with mtime for modification time
+        return fs.statSync(fullpath).ctime;
+    });
+}
+*/
+module.exports = router;
