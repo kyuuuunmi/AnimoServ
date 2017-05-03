2b460f21258e1fc73e3ba9ca05a4cc7fcc8019fc
yuuuunmi <kyuuuunmi@gmail.com>
2017-05-03 16:32:12 +0900

    fixed exec arg

--- routes/darknet.js
+++ routes/darknet.js
@@ -4,73 +4,79 @@ const fs = require('fs');
 const path = require('path');
 const _ = require('underscore');
 const async = require('async');
+const exec = require('child_process').exec;
 
-var exec = require('child_process').exec, child;
+router.get('/', function(req, res) {
 
-router.get('/', function(req, res){
+    var darknet_home = "/home/ubuntu/CNN/Animo_darknet/Animo_Darknet/Animo_Darknet";
 
-  var darknet_home ="/home/ubuntu/CNN/Animo_darknet/Animo_Darknet/Animo_Darknet";
-
-  // 1) 맨 마지막 raw image를 불러온다 & 명령행 인자 선언
-  var raw_image = getMostRecentFileName('/home/ubuntu/CNN/raw_image');
-  /*var arg_darknet = darknet_home + "/darknet detector test "+
+    // 1) 맨 마지막 raw image를 불러온다 & 명령행 인자 선언
+    var raw_image = getMostRecentFileName('/home/ubuntu/CNN/raw_image');
+    /*var arg_darknet = darknet_home + "/darknet detector test "+
                     darknet_home + "/cfg/voc.data " +
                     darknet_home + "/cfg/tiny-yolo-voc.cfg " +
                     darknet_home + "/tiny-yolo-voc.weights " + raw_image;
 */
-  var arg_darknet = "./darknet detector test /cfg/voc.data cfg/tiny-yolo-voc.cfg tiny-yolo-voc.weights " + raw_image;
+    var arg_darknet = "./darknet detector test /cfg/voc.data cfg/tiny-yolo-voc.cfg tiny-yolo-voc.weights " + raw_image;
 
 
-  var arg_mv = "mv "+ darknet_home + "/predictions.png /home/ubuntu/CNN/dt_image/"+ Date.now() + ".png";
+    var arg_mv = "mv " + darknet_home + "/predictions.png /home/ubuntu/CNN/dt_image/" + Date.now() + ".png";
 
 
-  async.series([
-    function(callback){
-      var arg_cd = "cd "+darknet_home;
+    async.series([
+      /*  function(callback) {
+            var arg_cd = "cd " + darknet_home;
 
-      child = exec(arg_cd, function (error, stdout, stderr) {
-          console.log('stdout: ' + stdout);
-          console.log('stderr: ' + stderr);
-          if (error !== null) {
-              console.log('exec error: ' + error);
-          }
-      });
-    },
 
-    // 2) 다크넷에 파일 인자를 넣어 실행.
-    function(callback){
+            child = exec(arg_cd, function(error, stdout, stderr) {
+                if (error !== null) {
+                    console.log('exec error: ' + error);
+                    return;
+                }
+                console.log('stdout: ' + stdout);
+                console.log('stderr: ' + stderr);
 
-      child = exec(arg_darknet, function (error, stdout, stderr) {
-          console.log('stdout: ' + stdout);
-          console.log('stderr: ' + stderr);
-          if (error !== null) {
-              console.log('exec error: ' + error);
-          }
-      });
-    },
-    // 3) output은 해당 경로로 이동시켜 준다.
-    function(callback){
+            });
+        },*/
 
-      child = exec(arg_mv, function (error, stdout, stderr) {
-          console.log('stdout: ' + stdout);
-          console.log('stderr: ' + stderr);
-          if (error !== null) {
-              console.log('exec error: ' + error);
-          }
-      });
-    }], function(err, result){
-      if (err)
-        console.log(err);
-      else
-        res.send("successed");
-  });
+        // 2) 다크넷에 파일 인자를 넣어 실행.
+        function(callback) {
+
+            child = exec(arg_darknet, {cwd:darknet_home}, function(error, stdout, stderr) {
+                if (error !== null) {
+                    console.log('exec error: ' + error);
+                }
+                console.log('stdout: ' + stdout);
+                console.log('stderr: ' + stderr);
+
+            });
+        },
+        // 3) output은 해당 경로로 이동시켜 준다.
+        function(callback) {
+
+            child = exec(arg_mv, {cwd:darknet_home},  function(error, stdout, stderr) {
+                if (error !== null) {
+                    console.log('exec error: ' + error);
+                }
+                console.log('stdout: ' + stdout);
+                console.log('stderr: ' + stderr);
+
+            });
+        }
+    ], function(err, result) {
+        if (err)
+            console.log(err);
+        else
+            res.send("successed");
+    });
 });
+
 function getMostRecentFileName(dir) {
     var files = fs.readdirSync(dir);
 
     // use underscore for max
     // 가장 최근에 변경된 파일을 리턴해줌
-    return path.join(dir, _.max(files, function (f) {
+    return path.join(dir, _.max(files, function(f) {
         var fullpath = path.join(dir, f); // 경로+파일 이름
 
         // ctime = creation time is used
