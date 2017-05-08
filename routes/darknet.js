dd80fdf14b413165076afc6501852f8735242158
yuuuunmi <kyuuuunmi@gmail.com>
2017-05-08 15:39:54 +0900

    cleaning logs

--- routes/darknet.js
+++ routes/darknet.js
@@ -11,7 +11,6 @@ const darknet_home = "/home/ubuntu/CNN/Animo_darknet/Animo_Darknet/Animo_Darknet
 router.get('/', function(req, res) {
     res.send("received");
     asyncLoop(10, function(loop) {
-      res.redirect
         execCNN(req, res, function(result) {
             console.log(loop.iteration());
             loop.next();
@@ -20,66 +19,52 @@ router.get('/', function(req, res) {
 });
 
 
+function execCNN(req, res, callback) {
+    // 1) 맨 마지막 raw image를 불러온다 & 명령행 인자 선언
+    var raw_image = getMostRecentFileName('/home/ubuntu/CNN/motion');
+    var arg_darknet = "./darknet detector test cfg/voc.data cfg/tiny-yolo-voc.cfg tiny-yolo-voc.weights " + raw_image;
+    var arg_mv = "mv " + darknet_home + "/predictions.png /home/ubuntu/CNN/dt_image/" + Date.now() + ".png";
 
-function execCNN(req, res, callback){
-  // 1) 맨 마지막 raw image를 불러온다 & 명령행 인자 선언
-  var raw_image = getMostRecentFileName('/home/ubuntu/CNN/motion');
-  var arg_darknet = "./darknet detector test cfg/voc.data cfg/tiny-yolo-voc.cfg tiny-yolo-voc.weights " + raw_image;
-  var arg_mv = "mv " + darknet_home + "/predictions.png /home/ubuntu/CNN/dt_image/" + Date.now() + ".png";
+    async.series([
+        // 2) 다크넷에 파일 인자를 넣어 실행.
+        function(cb) {
+            child = exec(arg_darknet, {
+                cwd: darknet_home
+            }, function(error, stdout, stderr) {
+                if (error !== null) {
+                    console.log('exec error: ' + error);
+                    cb('darknet err');
+                }
+                console.log('stdout: ' + stdout);
+                console.log('stderr: ' + stderr);
+                cb(null, '200 darknet');
+            });
 
+        },
+        // 3) output은 해당 경로로 이동시켜 준다.
+        function(cb) {
 
-//  while (1) {
-      async.series([
-          // 2) 다크넷에 파일 인자를 넣어 실행.
-          function(callback) {
-	     console.log("series1")
-              child = exec(arg_darknet, {
-                  cwd: darknet_home
-              }, function(error, stdout, stderr) {
-                  if (error) {
-                      console.log('exec error: ');
-		      console.log(error);
-                      callback('darknet err');
-                  }
-                  console.log('stdout: ' + stdout);
-                  console.log('stderr: ' + stderr);
-                  callback(null, '200 darknet');
+            //console.log(arg_mv);
+            child = exec(arg_mv, {
+                cwd: darknet_home
+            }, function(error, stdout, stderr) {
 
-              });
-             console.log('darknet!');
-             //callback(null, '200 darknet');
-          },
-          // 3) output은 해당 경로로 이동시켜 준다.
-          function(callback) {
-
-              console.log("series2"+arg_mv);
-              child = exec(arg_mv, {
-                  cwd: darknet_home
-              }, function(error, stdout, stderr) {
-                  //child = exec(arg_test, function(error, stdout, stderr) {
-                  if (error) {
-                      console.log('exec error: ' + error);
-                      callback('mv err');
-                  }
-                  console.log('stdout: ' + stdout);
-                  console.log('stderr: ' + stderr);
-                  callback(null, 'successed');
-              });
-              console.log('mv!');
-              //callback(null, '200 darknet');
-          }
-      ], function(err, result) {
-          if (err)
-              console.log(err);
-          else
-	     console.log("good");
-          //res.send("successed");
-      });
-      sleep.sleep(5);
-      callback();
-  //    setTimeout();
-//    
-
+                if (error !== null) {
+                    console.log('exec error: ' + error);
+                    cb('mv err');
+                }
+                console.log('stdout: ' + stdout);
+                console.log('stderr: ' + stderr);
+                cb(null, 'successed');
+            });
+        }
+    ], function(err, result) {
+        if (err)
+            console.log(err);
+        else
+            console.log("good");
+    });
+    setTimeout(callback, 3000);
 }
 
 
--- routes/darknet2.js
+++ routes/darknet2.js
@@ -4,79 +4,75 @@ const fs = require('fs');
 const path = require('path');
 const _ = require('underscore');
 const async = require('async');
-const sleep = require('sleep');
 const exec = require('child_process').exec;
 const darknet_home = "/home/ubuntu/CNN/Animo_darknet/Animo_Darknet/Animo_Darknet";
 
 router.get('/', function(req, res) {
     res.send("received");
     var count = 0;
-    async.whilst(function (){ return count < 5; },
-        function(callback){
-          count++;
-          // 1) 맨 마지막 raw image를 불러온다 & 명령행 인자 선언
-          var raw_image = getMostRecentFileName('/home/ubuntu/CNN/motion');
-          var arg_darknet = "./darknet detector test cfg/voc.data cfg/tiny-yolo-voc.cfg tiny-yolo-voc.weights " + raw_image;
-          var arg_mv = "mv " + darknet_home + "/predictions.png /home/ubuntu/CNN/dt_image/" + Date.now() + ".png";
-
-          async.series([
-              // 2) 다크넷에 파일 인자를 넣어 실행.
-              function(cb) {
-                  child = exec(arg_darknet,{cwd: darknet_home}, function(error, stdout, stderr) {
-                      if (error) {
-                          console.log('exec error: ')
-                          console.log(error);
-                          cb('darknet err');
-                      }
-                      console.log('stdout: ' + stdout);
-                      console.log('stderr: ' + stderr);
-                      cb(null, '200 darknet');
-
-                  });
-                  console.log('darknet!');
-                  //cb(null, '200 darknet');
-              },
-              // 3) output은 해당 경로로 이동시켜 준다.
-              function(cb) {
-                  console.log("mv : "+arg_mv);
-                  child = exec(arg_mv, {cwd: darknet_home}, function(error, stdout, stderr) {
-                      if (error) {
-                          console.log('exec error: ' + error);
-                          cb('mv err');
-                      }
-                      console.log('stdout: ' + stdout);
-                      console.log('stderr: ' + stderr);
-                      cb(null, 'successed');
-                  });
-                  console.log('mv');
-                  //cb(null, '200 darknet');
-              }
-          ], function(err, result) {
-              if (err)
-                  console.log(err);
-              else
-                  console.log("[darknet] try:"+count+" good");
-
-          });
-          setTimeout(callback, 3000);
+    async.whilst(
+        function() {
+            return count < 5;
         },
-        function (err) {
-          if(err){
-          console.log("[whilst err]");
-          console.log(err);}
-          else {
-            console.log("[ROUTE:darknet2] END");
-            return;
-          }
+        function(callback) {
+            count++;
+
+            // 1) 맨 마지막 raw image를 불러온다 & 명령행 인자 선언
+            var raw_image = getMostRecentFileName('/home/ubuntu/CNN/motion');
+            var arg_darknet = "./darknet detector test cfg/voc.data cfg/tiny-yolo-voc.cfg tiny-yolo-voc.weights " + raw_image;
+            var arg_mv = "mv " + darknet_home + "/predictions.png /home/ubuntu/CNN/dt_image/" + Date.now() + ".png";
+
+            async.series([
+                // 2) 다크넷에 파일 인자를 넣어 실행.
+                function(cb) {
+                  console.log('darknet : '+ arg_darknet);
+                    child = exec(arg_darknet, {
+                        cwd: darknet_home
+                    }, function(error, stdout, stderr) {
+                        if (error) {
+                            console.log('exec error: ');
+                            console.log(error);
+                            cb('darknet err');
+                        }
+                        console.log('stdout: ' + stdout);
+                        console.log('stderr: ' + stderr);
+                        cb(null, '200 darknet');
+                    });
+                },
+                // 3) output은 해당 경로로 이동시켜 준다.
+                function(cb) {
+                    console.log("mv : " + arg_mv);
+                    child = exec(arg_mv, {
+                        cwd: darknet_home
+                    }, function(error, stdout, stderr) {
+                        if (error) {
+                            console.log('exec error: ' + error);
+                            cb('mv err');
+                        }
+                        console.log('stdout: ' + stdout);
+                        console.log('stderr: ' + stderr);
+                        cb(null, 'successed');
+                    });
+                }
+            ], function(err, result) {
+                if (err)
+                    console.log(err);
+                else
+                    console.log("[darknet] try:" + count + " good");
+            });
+            setTimeout(callback, 3000);
+        },
+        function(err) {
+            if (err) {
+                console.log("[whilst err]");
+                console.log(err);
+            } else {
+                console.log("[ROUTE:darknet2] END");
+                return;
+            }
         }
     );
 });
-//
-// function execCNN(callback,count) {
-//
-// }
-//
-
 
 function getMostRecentFileName(dir) {
     var files = fs.readdirSync(dir);
